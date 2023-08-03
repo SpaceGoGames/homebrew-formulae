9a309eab1bbfddfe999c3c258313cbd82df4dc60
Moritz Wundke <b.thax.dcg@gmail.com>
2023-08-03 11:45:45 +0200

    Update osx-ca-bundler.rb
    Adding sha256 for release

--- Formula/osx-ca-bundler.rb
+++ Formula/osx-ca-bundler.rb
@@ -2,7 +2,7 @@ class OsxCaBundler < Formula
   desc "Tool that bundles together all keychain system certs into one CA bundle to be used together"
   homepage "https://github.com/SpaceGoGames/osx-ca-bundler"
   url "https://github.com/SpaceGoGames/osx-ca-bundler/archive/v0.0.5.tar.gz"
-  sha256 ""
+  sha256 "47b5edb607627071ccf07159ef039dacc3ff12560d61e850a78c05df74833521"
   version "0.0.5"
   license "MIT"
 
