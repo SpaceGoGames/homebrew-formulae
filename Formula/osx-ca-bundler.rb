eefd4a7b8781d2f0c7f9ec2de199e5b1aa64f714
Moritz Wundke <b.thax.dcg@gmail.com>
2023-08-04 16:21:10 +0200

    osx-ca-bundler 1.1.0

--- Formula/osx-ca-bundler.rb
+++ Formula/osx-ca-bundler.rb
@@ -1,9 +1,9 @@
 class OsxCaBundler < Formula
   desc "Tool that bundles together all keychain system certs into one CA bundle to be used together"
   homepage "https://github.com/SpaceGoGames/osx-ca-bundler"
-  url "https://github.com/SpaceGoGames/osx-ca-bundler/archive/v1.0.0.tar.gz"
-  sha256 "eff396c7efa9a5ebf31721868d12f124b6a0c9cdca80c7a5c08291374daf5dd7"
-  version "1.0.0"
+  url "https://github.com/SpaceGoGames/osx-ca-bundler/archive/v1.1.0.tar.gz"
+  sha256 "0991275cdfdb54391d2b156f13c53368f79924ea45effcb0550a3188beb9e3cc"
+  version "1.1.0"
   license "MIT"
 
   depends_on xcode: [">= 14.3", :build]
